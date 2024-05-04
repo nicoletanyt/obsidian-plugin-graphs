12026340dc2f5993cf3e93e007f506b7794c1fea
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-05-04 17:54:47 +0800

    fixed error of vectors not showing up after graphs (code is a lil messy)

--- GraphViews.tsx
+++ GraphViews.tsx
@@ -4,46 +4,49 @@ import functionPlot from "function-plot";
 
 interface GraphProps {
 	input: string;
-    vectorArray: number[]
+	vectorArray?: number[];
 }
 
 function Graph({ input, vectorArray }: GraphProps) {
+
 	useEffect(() => {
 		console.log("Input: " + input);
-        
-        if (vectorArray) {
-            functionPlot({
-                target: '#graph',
-                xAxis: { domain: [-3, 8] },
-                grid: true,
-                data: [
-                  {
-                    vector: [vectorArray[0], vectorArray[1]],
-                    offset: [0, 0],
-                    graphType: 'polyline',
-                    fnType: 'vector'
-                  }
-                ]
-              })
-        } else if (input){
-            functionPlot({
-                target: "#graph",
-                width: 800,
-                height: 500,
-                yAxis: { domain: [-1, 9] },
-                grid: true,
-                data: [
-                    {
-                        fn: input,
-                        // derivative: {
-                        //     fn: "2 * x",
-                        //     updateOnMouseMove: true
-                        // }
-                    },
-                ],
-            });
-        } else {
-            console.log("Loading...")
+
+		if (input.includes("vec")) {
+            if (vectorArray && vectorArray.length > 0) {
+                functionPlot({
+                    target: "#graph",
+                    xAxis: { domain: [-3, 8] },
+                    grid: true,
+                    data: [
+                        {
+                            vector: [vectorArray[0], vectorArray[1]],
+                            offset: [0, 0],
+                            graphType: "polyline",
+                            fnType: "vector",
+                        },
+                    ],
+                });
+            }
+		} else if (input.includes("x")){
+			functionPlot({
+				target: "#graph",
+				width: 800,
+				height: 500,
+				yAxis: { domain: [-1, 9] },
+				grid: true,
+				data: [
+					{
+						fn: input,
+						// derivative: {
+						//     fn: "2 * x",
+						//     updateOnMouseMove: true
+						// }
+					},
+				],
+			});
+		} else {
+            console.log("Undefined Type")
         }
 	}, [input]);
 
@@ -54,24 +57,43 @@ interface GraphWrapperProps {
 	functionInput: string;
 }
 
-const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput }) => {
+enum GraphType {
+	Vector = "VECTOR",
+	Graph = "GRAPH",
+    NULL = "NULL"
+}
 
-    const [vector, setVector] = useState<number[]>([]);
+const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput }) => {
+	const [vector, setVector] = useState<number[]>([]);
+	const [type, setType] = useState<GraphType>(GraphType.NULL);
 
 	useEffect(() => {
 		console.log("Function Input: " + functionInput);
+
         if (functionInput.includes("vec")) {
-            // EXAMPLE: \vec{AB} = \begin{pmatrix} 1 \\ 2 \end{pmatrix}
-            let column = functionInput.split("\\begin{pmatrix}")[1].replace("\\end{pmatrix}", "").replace(/\s+/g, "")
-            // COLUMN SHOULD BE SOMETHING LIKE "1 \\ 2 \"
-            let rawVector = column.split("\\")
-            let vectorParsed = []
-            
-            for (let i = 0; i < rawVector.length; ++i) {
-                if (rawVector[i] != '') vectorParsed.push(Number(rawVector[i]))
-            }
-            setVector(vectorParsed)
+            setType(GraphType.Vector);
+        } else if (functionInput == "") {
+            setType(GraphType.NULL)
+        } else {
+            setType(GraphType.Graph);
         }
+
+		if (functionInput.includes("vec")) {
+			// EXAMPLE: \vec{AB} = \begin{pmatrix} 1 \\ 2 \end{pmatrix}
+			let column = functionInput
+				.split("\\begin{pmatrix}")[1]
+				.replace("\\end{pmatrix}", "")
+				.replace(/\s+/g, "");
+			// COLUMN SHOULD BE SOMETHING LIKE "1 \\ 2 \"
+			let rawVector = column.split("\\");
+			let vectorParsed = [];
+
+			for (let i = 0; i < rawVector.length; ++i) {
+				if (rawVector[i] != "") vectorParsed.push(Number(rawVector[i]));
+			}
+			setVector(vectorParsed);
+		} 
+
 	}, [functionInput]);
 
 	function getAnswer() {
@@ -91,15 +113,25 @@ const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput }) => {
 	return functionInput ? (
 		<div>
 			<h1>Graph of {functionInput}</h1>
-            {vector.length > 0 ? <Graph input={functionInput} vectorArray={vector}/> : <p>Loading...</p>}
-			<h2>Input the value of x: </h2>
-			<input id="x-val" type="text" />
-			<button onClick={getAnswer}>Enter</button>
-			<h2 id="y-res"></h2>
+			{type == "GRAPH" ? (
+				<Graph input={functionInput}/>
+			) : type == "VECTOR" && vector.length > 0 ? (
+				<Graph input={functionInput} vectorArray={vector}/>
+			) : (
+				<p>Loading...</p>
+			)}
+			{type == "GRAPH" ? (
+				<>
+					<h2>Input the value of x: </h2>
+					<input id="x-val" type="text" />
+					<button onClick={getAnswer}>Enter</button>
+					<h2 id="y-res"></h2>
+				</>
+			) : null}
 		</div>
 	) : (
 		<div>
-			<h1>Use the command to generate a graph.</h1>
+			<h1>No function entered.</h1>
 		</div>
 	);
 };
