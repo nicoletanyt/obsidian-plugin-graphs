0ee21f2f1305c004f2f4689eb4d508183097f0f3
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-05-06 11:42:32 +0800

    added ability to generate the addition vector

--- GraphViews.tsx
+++ GraphViews.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import functionPlot, { FunctionPlotDatum } from "function-plot";
+import functionPlot, { FunctionPlotDatum, FunctionPlotOptions } from "function-plot";
 import { Notice } from "obsidian";
 
 interface GraphProps {
@@ -82,6 +82,22 @@ interface GraphWrapperProps {
 const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput, type }) => {
 	const [vector, setVector] = useState<number[][]>([]);
 
+    function parseVector(func: string) {
+        let column = func
+            .split("\\begin{pmatrix}")[1]
+            .replace("\\end{pmatrix}", "")
+            .replace(/\s+/g, "");
+        // COLUMN SHOULD BE SOMETHING LIKE "1 \\ 2 \"
+        let rawVector = column.split("\\");
+        let vectorParsed = [];
+
+        for (let i = 0; i < rawVector.length; ++i) {
+            if (rawVector[i] != "")
+                vectorParsed.push(Number(rawVector[i]));
+        }
+        return vectorParsed
+    }
+
 	useEffect(() => {
 		console.log("Function Inputs: " + functionInput);
 
@@ -90,19 +106,8 @@ const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput, type }) => {
 			for (let i = 0; i < functionInput.length; ++i) {
 				// EXAMPLE: \vec{AB} = \begin{pmatrix} 1 \\ 2 \end{pmatrix}
 				console.log(i + "\t" + functionInput[i]);
-				let column = functionInput[i]
-					.split("\\begin{pmatrix}")[1]
-					.replace("\\end{pmatrix}", "")
-					.replace(/\s+/g, "");
-				// COLUMN SHOULD BE SOMETHING LIKE "1 \\ 2 \"
-				let rawVector = column.split("\\");
-				let vectorParsed = [];
-
-				for (let i = 0; i < rawVector.length; ++i) {
-					if (rawVector[i] != "")
-						vectorParsed.push(Number(rawVector[i]));
-				}
-				tempVectors.push(vectorParsed);
+				
+				tempVectors.push(parseVector(functionInput[i]));
 			}
 			setVector(tempVectors);
 		}
@@ -124,6 +129,54 @@ const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput, type }) => {
 		if (yText) yText.innerText = "y = " + y.toString();
 	}
 
+    function getAdditionVector() {
+        const vector1 = parseVector((document.getElementById("vector-1") as HTMLInputElement).value)
+        const vector2 = parseVector((document.getElementById("vector-2") as HTMLInputElement).value)
+        const offset = vector1 // let position of vector 1 be offset, aka the "origin" of the vector
+        let result = [vector2[0] - vector1[0], vector2[1] - vector1[1]]
+        let data = []
+        
+        if (result[0] == 0 && result[1] == 0) {
+            // zero vector
+            console.log("Please select two different vectors.")
+            new Notice("Please select two different vectors. ")
+        } else {
+            // assume they have a common point
+            const resultText = document.getElementById("result-vector")
+            if (resultText) resultText.innerText = "The Resultant Vector is: " + result + ". (" + COLORS[functionInput.length] + ")"
+
+            for (let i = 0; i < functionInput.length; ++i) {
+                console.log(parseVector(functionInput[i]))
+                data.push({
+                    vector: parseVector(functionInput[i]),
+                    offset: [0, 0],
+                    graphType: "polyline",
+                    fnType: "vector",
+                    color: COLORS[i]
+                })
+            }
+            // push resultant vector 
+            data.push({
+                vector: result,
+                offset: offset,
+                graphType: "polyline",
+                fnType: "vector",
+                color: COLORS[functionInput.length]
+            })
+            console.log(data)
+
+            functionPlot({
+                target: "#graph",
+                xAxis: { domain: [-3, 8] },
+                grid: true,
+                width: 800,
+                height: 550,
+                data: data as FunctionPlotDatum[]
+            });
+        }   
+
+    }
+
 	return functionInput ? (
 		<div>
 			<h1>Graph of {functionInput}</h1>
@@ -140,7 +193,8 @@ const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput, type }) => {
 				))}
 			</ul>
 			{type == "GRAPH" ? (
-				<div>
+                <div>
+                    <hr/>
 					<h3>Pick a function to calculate. </h3>
                     <select id="pick-functions" name="functions">
                         {functionInput.map((graph, index) => (
@@ -153,6 +207,32 @@ const GraphWrapper: React.FC<GraphWrapperProps> = ({ functionInput, type }) => {
 					<h2 id="y-res"></h2>
                 </div>
 			) : null}
+            { type == "VECTOR" ? (
+                <div>
+                    <hr/>
+                    <h2>Select two vectors to add up. </h2>
+                    <div className="vector-container">
+                        <p>Vector 1: </p>
+                        <select id="vector-1" name="vector-1">
+                            {functionInput.map((graph, index) => (
+                                <option key={index} value={graph}>{graph}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="vector-container">
+                        <p>Vector 2: </p>
+                        <select id="vector-2" name="vector-2">
+                            {functionInput.map((graph, index) => (
+                                <option key={index} value={graph}>{graph}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <br></br>
+                    <button onClick={getAdditionVector}>Generate addition vector. </button>
+                    <p id="result-vector"></p>
+                </div> 
+            ) : <></>
+            }
 		</div>
 	) : (
 		<div>
